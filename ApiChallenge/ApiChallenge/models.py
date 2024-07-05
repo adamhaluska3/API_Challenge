ed418b172e428f5dd90c74845ce427da86307e9d
Adam Haluška <adam.haluska3@gmail.com>
2024-07-05 21:14:31 +0200

    Models update

--- ApiChallenge/ApiChallenge/models.py
+++ ApiChallenge/ApiChallenge/models.py
@@ -1,23 +1,5 @@
-from typing import str, List
+from typing import str, List, Self
 from django.db import models
-
-""" Database models"""
-class Country(models.Model):
-    id = models.AutoField(primary_key=True, editable=False)
-    createdAt = models.DateTimeField()
-    groupId = models.IntegerField()  # TODO: Ask for explanation of usage
-    
-    countryCreated = models.ForeignKey(on_delete=models.CASCADE)
-    
-    REQUIRED_FIELDS = ["id", "groupId"]
-
-
-class CountryCreated(models.Model):
-    name = models.CharField()
-    countryCode = models.CharField()
-    
-    REQUIRED_FIELDS = ["name", "countryCode"]
-    
 
 """ Utility models """
 class Links:
@@ -35,8 +17,27 @@ class CollectionResult:
     def __init__(self, links: Links, pagination: Pagination) -> None:
         self.links = links
         self.pagination = pagination
+
+        
+class CountryCreated:
+    def __init__(self, name: str, countryCode: str) -> None:
+        self.name = name
+        self.countryCode = countryCode
         
 class Countries:
     def __init__(self, collectionResult: CollectionResult, countries: List[Country]) -> None:
         self.collectionResult = collectionResult
         self.countries = countries
+
+   
+   
+""" Database models""" 
+class Country(models.Model):
+    id = models.AutoField(primary_key=True, editable=False)
+    createdAt = models.DateTimeField()
+    groupId = models.IntegerField()  # TODO: Ask for explanation of usage
+    
+    name = models.CharField()
+    countryCode = models.CharField()
+    
+    REQUIRED_FIELDS = ["id", "groupId"]
