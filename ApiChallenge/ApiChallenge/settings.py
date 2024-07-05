ccaf4df6ec8643997b355290ffad110c67d16d63
Adam Haluška <adam.haluska3@gmail.com>
2024-07-05 22:27:38 +0200

    Add parameters' max_length, add project to installed_apps

--- ApiChallenge/ApiChallenge/settings.py
+++ ApiChallenge/ApiChallenge/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'ApiChallenge',
+    # 'ApiChallenge.models',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
