1a951695fb88ac25f0b67ef24dd089d383e92e2e
Adam Haluška <adam.haluska3@gmail.com>
2024-07-05 22:51:38 +0200

    Create App and move data, Add Serializers & Views

---
+++ ApiChallenge/ApiBackend/models.py
@@ -0,0 +1,43 @@
+from typing import List, Self
+from django.db import models
+
+""" Utility models """
+class Links:
+    def __init__(self, previous: str, next: str) -> None:
+        self.previous = previous
+        self.next = next
+    
+class Pagination:
+    def __init__(self, count: int, offset: int, limit: int) -> None:
+        self.count = count
+        self.offset = offset
+        self.limit = limit
+    
+class CollectionResult:
+    def __init__(self, links: Links, pagination: Pagination) -> None:
+        self.links = links
+        self.pagination = pagination
+
+        
+class CountryCreated:
+    def __init__(self, name: str, countryCode: str) -> None:
+        self.name = name
+        self.countryCode = countryCode
+        
+class Countries:
+    def __init__(self, collectionResult: CollectionResult, countries: List['Country']) -> None:
+        self.collectionResult = collectionResult
+        self.countries = countries
+
+   
+   
+""" Database models""" 
+class Country(models.Model):
+    id = models.AutoField(primary_key=True, editable=False)
+    createdAt = models.DateTimeField()
+    groupId = models.IntegerField()  # TODO: Ask for explanation of usage
+    
+    name = models.CharField(max_length=64)
+    countryCode = models.CharField(max_length=16)
+    
+    REQUIRED_FIELDS = ["id", "groupId"]
