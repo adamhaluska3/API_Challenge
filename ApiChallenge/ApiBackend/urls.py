e5ab1ee4310ffd834208ce8cf927bf47cf8bc226
Adam Haluška <adam.haluska3@gmail.com>
2024-07-06 10:23:23 +0200

    Update models, Add serializers and URL paths

--- ApiChallenge/ApiBackend/urls.py
+++ ApiChallenge/ApiBackend/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<int:country_id>", views.CountriesIdentifiableView.as_view(), name="yippee")
+    path("", views.CountriesBaseView.as_view(), name=views.CountriesBaseView.__name__),
+    path("<int:country_id>", views.CountriesIdentifiableView.as_view(), name=views.CountriesIdentifiableView.__name__)
 ]
--- ApiChallenge/ApiBackend/views.py
+++ ApiChallenge/ApiBackend/views.py
@@ -1,23 +1,56 @@
 from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from json import loads
+
+from datetime import datetime
 
 from . import serializers
 from . import models
 
 
+def getCountryById(countryId: int) -> models.Country | None:
+    try:
+        return models.Country.objects.get(id = countryId)
+    except models.Country.DoesNotExist:
+        return None
+
+
 class CountriesIdentifiableView(APIView):
     def get(self, request, country_id):
-        try:
-            country = models.Country.objects.get(id = country_id)
-        except models.Country.DoesNotExist:
-            return Response(status=400)
+        country = getCountryById(country_id)
+        if country is None:
+            return Response(status=404)
         
         serializer = serializers.CountrySerializer(country)
         return Response(serializer.data)
+    
+    def put(self, request, country_id):
+        country = getCountryById(country_id)
+        if country is None:
+            return Response(status=404)
+        
+        serializer = serializers.CountrySerializer(country, data=request.body, partial=True)
+        outputCountryCreate = models.CountryCreate(serializer.data["name"], serializer.data["countryCode"])
+        return Response(outputCountryCreate)
+
+class CountriesBaseView(APIView):    
+    def get(self, request):
+        return
+    
+    def post(self, request):
+        data = loads(request.body)
+        data["createdAt"] = datetime.now()
+        data["groupId"]  = 1
+        
+        serializer = serializers.CountrySerializer(data=data)
+        
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(status=500)
         
         
-        
-# class CountriesBaseView(APIView):
     
         
