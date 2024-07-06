d7c04977738dbd388e043ba0b93c9a7525d2c453
Adam Haluška <adam.haluska3@gmail.com>
2024-07-07 00:25:41 +0200

    Remove unused models

--- ApiChallenge/ApiBackend/models.py
+++ ApiChallenge/ApiBackend/models.py
@@ -1,36 +1,6 @@
 from typing import List, Self
 from django.db import models
 
-""" Utility models """
-class Links:
-    def __init__(self, previous: str, next: str) -> None:
-        self.previous = previous
-        self.next = next
-    
-class Pagination:
-    def __init__(self, count: int, offset: int, limit: int) -> None:
-        self.count = count
-        self.offset = offset
-        self.limit = limit
-    
-class CollectionResult:
-    def __init__(self, links: Links, pagination: Pagination) -> None:
-        self.links = links
-        self.pagination = pagination
-
-        
-class CountryCreate:
-    def __init__(self, name: str, countryCode: str) -> None:
-        self.name = name
-        self.countryCode = countryCode
-        
-class Countries:
-    def __init__(self, collectionResult: CollectionResult, countries: List['Country']) -> None:
-        self.collectionResult = collectionResult
-        self.countries = countries
-
-   
-   
 """ Database models""" 
 class Country(models.Model):
     id = models.AutoField(primary_key=True, editable=False)
