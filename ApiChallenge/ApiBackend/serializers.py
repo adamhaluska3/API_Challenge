5df665d4a1622301c9477397e3d1fc3fc58c7bc8
Adam Haluška <adam.haluska3@gmail.com>
2024-07-06 18:45:33 +0200

    Remove unused create, Add new serializer

--- ApiChallenge/ApiBackend/serializers.py
+++ ApiChallenge/ApiBackend/serializers.py
@@ -4,21 +4,15 @@ from datetime import datetime
 
 from . import models
 
+class CountryCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.CountryCreate
+        fields = "__all__"
 
 class CountrySerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Country
         fields = "__all__"
-    
-    def createCountry(self, countryCreate: models.CountryCreate, 
-                      countryId: int, groupId: int) -> Self:
-        newCountry = models.Country.objects.create(id = countryId,
-                                            createdAt = datetime.now(),
-                                            groupId = groupId,
-                                            name = countryCreate.name,
-                                            countryCode = countryCreate.countryCode)
-        newCountry.save()
-        return newCountry
 
                 
         
