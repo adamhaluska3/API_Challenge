81e9321a19617bf8796cf8e25317ff3064abbb0e
Adam Haluška <adam.haluska3@gmail.com>
2024-07-08 01:44:07 +0200

    Add docstrings, Format code

--- ApiChallenge/ApiBackend/countriesPaginator.py
+++ ApiChallenge/ApiBackend/countriesPaginator.py
@@ -1,17 +1,45 @@
+from rest_framework.request import Request
 from rest_framework.pagination import LimitOffsetPagination
 
+from typing import List, Dict, Any
+
+from .models import Country
+
+
+defaultPaginationLimit: int = 50  # TODO move ot constants?
+
 class CountriesPaginator(LimitOffsetPagination):
+    """
+    Custom paginator based on native LimitOffsetPaginator. 
+    
+    Provides:
+        URLs for next/previous request,
+        pagination data (countr, offset and limit).    
+    """
     limit_query_param = "PageLimit"
     offset_query_param = "PageOffset"
+
+
+    def getpaginatedData(self, request: Request, countriesData: List[Country]) -> Dict[str:Any]:
+        """
+        Paginates the ``countriesData`` using the values in ``request``.
+
+        Parameters:
+            request (Request): HTTP request.
+            countriesData (List[Country]): List of countries to be used as pagination source.
+
+        Returns:
+            Dict[str:Any]: Dictionary containing prepared data for serialization of Countries object.
+        """
         
-    def getpaginatedData(self, request, countriesData):
+        # Values required for 'paginate_queryset' to be set.
         self.request = request
         
         self.count = len(countriesData)
         self.offset = int(request.query_params.get("PageOffset", "0"))
         
         limit = request.query_params.get("PageLimit")
-        self.limit = int(limit) if (limit is not None) else 20
+        self.limit = int(limit) if (limit is not None) else defaultPaginationLimit
         
         links = {
             "previous": self.get_previous_link(),
--- ApiChallenge/ApiBackend/serializers.py
+++ ApiChallenge/ApiBackend/serializers.py
@@ -6,20 +6,50 @@ from . import models
 
 
 class CountrySerializer(serializers.ModelSerializer):
+    """
+    Serializer for Country model.
+    
+    Fields:
+        Same as 'Country' model.
+    """
     class Meta:
         model = models.Country
         fields = "__all__"
         
 class LinksSerializer(serializers.Serializer):
+    """
+    Serializer for a Links object.
+
+    Fields:
+        next (URL): URL address (GET request) for following entries.
+        previous (URL): URL address (GET request) for previous entries.
+    """
     next = serializers.URLField(allow_null=True, required=False)
     previous = serializers.URLField(allow_null=True, required=False)
 
 class PaginationSerializer(serializers.Serializer):
+    """
+    Serializer for a Pagination object.
+
+    Fields:
+        count (int): Total number of suitable entries.
+        offset (int): Index of a first entry in a HTTP response.
+        limit (int): Maximal number of entries in a HTTP response.
+    """
     count = serializers.IntegerField()
     offset = serializers.IntegerField()
     limit = serializers.IntegerField()
     
 class CountriesSerializer(serializers.Serializer):
+    """
+    Serializer for a Countries object.
+
+    Fields:
+        links (Links): Object with URL addresses for retrieving previous/next list of Countries.
+        pagination (Pagination): Object with data of total amount, starting index 
+                                 and amount of returned countries.
+        countries (List[Country]): List of countries.
+    """
     links = LinksSerializer()
     pagination = PaginationSerializer()
     countries = CountrySerializer(many=True)
