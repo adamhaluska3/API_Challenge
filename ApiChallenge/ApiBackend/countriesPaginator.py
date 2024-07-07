d06765802ff7468bb715f1e34f10a4e8c9782273
Adam Haluška <adam.haluska3@gmail.com>
2024-07-07 20:56:35 +0200

    Rename limit and offset parameters

--- ApiChallenge/ApiBackend/countriesPaginator.py
+++ ApiChallenge/ApiBackend/countriesPaginator.py
@@ -1,29 +1,31 @@
 from rest_framework.pagination import LimitOffsetPagination
 
 class CountriesPaginator(LimitOffsetPagination):
+    limit_query_param = "PageLimit"
+    offset_query_param = "PageOffset"
         
-        def getpaginatedData(self, request, countriesData):
-            self.request = request
-            
-            self.count = len(countriesData)
-            self.offset = int(request.query_params.get("offset", "0"))
-            
-            limit = request.query_params.get("limit")
-            self.limit = int(limit) if (limit is not None) else 20
-            
-            links = {
-                "previous": self.get_previous_link(),
-                "next": self.get_next_link()
-            }
-            
-            pagination = {
-                "count": self.count,
-                "offset": self.offset,
-                "limit": self.limit
-            }
-            
-            return {
-                "links": links,
-                "pagination": pagination,
-                "countries": self.paginate_queryset(countriesData, request)
-            }
+    def getpaginatedData(self, request, countriesData):
+        self.request = request
+        
+        self.count = len(countriesData)
+        self.offset = int(request.query_params.get("PageOffset", "0"))
+        
+        limit = request.query_params.get("PageLimit")
+        self.limit = int(limit) if (limit is not None) else 20
+        
+        links = {
+            "previous": self.get_previous_link(),
+            "next": self.get_next_link()
+        }
+        
+        pagination = {
+            "count": self.count,
+            "offset": self.offset,
+            "limit": self.limit
+        }
+        
+        return {
+            "links": links,
+            "pagination": pagination,
+            "countries": self.paginate_queryset(countriesData, request)
+        }
