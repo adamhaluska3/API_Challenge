a8703638dde323ff1aef52b70aed0433477294e8
Adam Haluška <adam.haluska3@gmail.com>
2024-07-07 23:07:20 +0200

    Fix URL routing

--- ApiChallenge/ApiBackend/urls.py
+++ ApiChallenge/ApiBackend/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.CountriesBaseView.as_view(), name=views.CountriesBaseView.__name__),
-    path("<int:country_id>", views.CountriesIdentifiableView.as_view(), name=views.CountriesIdentifiableView.__name__)
+    path("countries", views.CountriesBaseView.as_view(), name=views.CountriesBaseView.__name__),
+    path("countries/<int:country_id>", views.CountriesIdentifiableView.as_view(), name=views.CountriesIdentifiableView.__name__)
 ]
